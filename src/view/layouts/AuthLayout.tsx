bd6206915768d28d30a88c8495a62d24bfbde47f
pedrobadm7 <pedrobars7a@gmail.com>
2023-09-25 21:22:22 -0300

    feature: create Logo component

--- src/view/layouts/AuthLayout.tsx
+++ src/view/layouts/AuthLayout.tsx
@@ -1,4 +1,5 @@
 import illustration from '../../assets/images/illustration.png'
+import { Logo } from '../components/Logo'
 
 export function AuthLayout() {
   return (
@@ -8,11 +9,12 @@ export function AuthLayout() {
       <div className='w-1/2 h-full flex justify-center items-center p-8 relative'>
         <img
           src={illustration}
-          className=' object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]'
+          className='object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]'
         />
 
-        <div className="max-w-[656px] bottom-8 bg-white p-10 absolute rounded-b-[32px]">
-          <p>
+        <div className="max-w-[656px] bottom-8 bg-white p-10 absolute rounded-b-[32px] text-teal-900">
+          <Logo />
+          <p className="text-gray-700 font-medium text-xl mt-6">
             Gerencie suas finanças pessoais de uma forma simples com o fincheck, e o melhor, totalmente de graça!
           </p>
         </div>
