3e76d893c974e47022afeb87343aea3b31ed7548
pedrobadm7 <pedrobars7a@gmail.com>
2023-09-25 23:17:36 -0300

    chore: create register screen

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -1,5 +1,39 @@
+import { Link } from 'react-router-dom';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
 export function Register() {
   return (
-    <h1>Register</h1>
+    <>
+      <header className="flex flex-col items-center gap-4" >
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Crie sua conta
+        </h1>
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">Já possui uma conta?</span>
+          <Link className="tracking-[-0.5px] font-medium text-teal-900" to="/login">Fazer Login</Link>
+        </p>
+
+      </header>
+
+      <form action="" className="mt-[60px] flex flex-col gap-4">
+        <Input
+          type="name"
+          placeholder="Nome"
+          name="name"
+        />
+        <Input
+          type="email"
+          placeholder="E-mail"
+          name="email"
+        />
+        <Input
+          type="password"
+          placeholder="Senha"
+          name="password"
+        />
+        <Button type="submit" className="mt-2">Criar conta</Button>
+      </form>
+    </>
   )
 }
