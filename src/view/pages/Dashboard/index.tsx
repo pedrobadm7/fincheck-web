c022db24374121519103c7bf42795cd1fdf72d09
pedrobadm7 <pedrobars7a@gmail.com>
2023-09-29 12:01:04 -0300

    feature: create accounts stuffs

--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -1,17 +1,24 @@
 import { Logo } from '../../components/Logo';
 import { UserMenu } from '../../components/UserMenu';
+import { Accounts } from './components/Accounts';
+import { Transactions } from './components/Transactions';
 
 export function Dashboard() {
 
   return (
-    <div className="h-full w-full px-8 pb-8 pt-6 flex flex-col">
+    <div className="h-full w-full p-4 md:px-8 md:pb-8 md:pt-6 flex flex-col">
       <header className="h-12 flex justify-between items-center">
         <Logo className="h-6 text-teal-900" />
         <UserMenu />
       </header>
 
-      <main>
-        Content
+      <main className="flex-1 flex gap-4 flex-col md:flex-row">
+        <div className="w-full md:m-1/2">
+          <Accounts />
+        </div>
+        <div className="w-full md:m-1/2">
+          <Transactions />
+        </div>
       </main>
     </div>
   )
